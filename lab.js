67627a6ec8125c2653617eb53c6ccf2a2fac83cf
KhanTheDev <153256283+KhanTheDev@users.noreply.github.com>
2025-07-30 15:44:08 -0400

    Update lab.js

--- lab.js
+++ lab.js
@@ -51,5 +51,11 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
   // Part 1.2 
 
-  
+  const firstVsLast = n1 > n4;
+
+  // Part 1.3 
+
+  console.log(((n1 - n2) * n3) & n4);
+
+  // Part 1.4
   
